fd480e653046ccd074ec3fed266d8e93b0087c0e
n.zarubin <n.zarubin@itsirius.ru>
2020-03-12 11:29:33 +0300

    m

--- Client.php
+++ Client.php
@@ -1,11 +1,58 @@
 <?php
 
 namespace nikserg\CRMCertificateAPI;
-class Client {
+
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Exception\RequestException;
+use nikserg\CRMCertificateAPI\models\out\CustomerForm;
+use Psr\Http\Message\ResponseInterface;
+
+class Client
+{
+
+    public const PRODUCTION_URL = 'https://crm.uc-itcom.ru/index.php/';
+    public const TEST_URL = 'https://dev.uc-itcom.ru/index.php/';
+
+    private const ACTION_ADD_CUSTOMER_FORM = 'gateway/itkExchange/pushCustomerForm';
 
     protected $apiKey;
-    public function __construct($apiKey)
+    protected $url;
+    protected $guzzle;
+
+    public function __construct($apiKey, $url = self::PRODUCTION_URL)
     {
         $this->apiKey = $apiKey;
+        $this->url = $url;
+        $this->guzzle = new \GuzzleHttp\Client();
+    }
+
+    private function getJsonBody(ResponseInterface $response) {
+        $body = $response->getBody();
+        if (!$body) {
+            throw new  \Exception('Получено пустое тело ответа на запрос');
+        }
+        $json = @json_decode($body);
+        if (!$json) {
+            throw new  \Exception('Невозможно распарсить ответ '.print_r($body, true));
+        }
+        return $json;
+    }
+
+    /**
+     * @param CustomerForm $customerForm
+     * @return int ID заявки в CRM
+     */
+    public function sendCustomerForm(CustomerForm $customerForm)
+    {
+        $url = $this->url . self::ACTION_ADD_CUSTOMER_FORM;
+
+        $postData = [
+            'form_params' => (array)$customerForm,
+        ];
+        print_r($postData);
+        exit;
+        $result = $this->guzzle->post($url, $postData);
+        $result = $this->getJsonBody($result);
+        return $result['zurmo-id'];
     }
 }
---
+++ models/out/CustomerForm.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: n.zarubin
+ * Date: 12.03.2020
+ * Time: 11:05
+ */
+
+namespace nikserg\CRMCertificateAPI\models\out;
+
+/**
+ * Заявка на сертификат в CRM
+ * Модель для передачи в CRM
+ *
+ * @package nikserg\CRMCertificateAPI\models\out
+ */
+class CustomerForm
+{
+    //
+    // Возможные значения customerLegalForm
+    //
+    const CUSTOMER_LEGAL_FORM_LEGAL = 'legal';           //Юр. лицо
+    const CUSTOMER_LEGAL_FORM_INDIVIDUAL = 'individual'; //ИП
+    const CUSTOMER_LEGAL_FORM_PERSON = 'person';         //Физ. лицо
+
+
+    //
+    // Криптопровайдеры
+    //
+    const CRYPTOPROVIDER_CRYPTOPRO = 'CRYPTO_PRO';
+    const CRYPTOPROVIDER_CRYPTOPRO_2012 = 'CRYPTO_PRO_2012'; //По умолчанию
+    const CRYPTOPROVIDER_JACARTA = 'JA_CARTA';
+    const CRYPTOPROVIDER_RUTOKEN_GOST = 'RUTOKEN_GOST';
+    const CRYPTOPROVIDER_VIPNET = 'VIPNET';
+    const CRYPTOPROVIDER_VIPNET_2012 = 'VIPNET_2012';
+    const CRYPTOPROVIDER_ESMART = 'ESMART';
+    const CRYPTOPROVIDER_ESMART_2012 = 'ESMART_2012';
+    const CRYPTOPROVIDER_JACARTA_2012 = 'JA_CARTA_2012';
+    const CRYPTOPROVIDER_RUTOKEN_GOST_2012 = 'RUTOKEN_GOST_2012';
+    const CRYPTOPROVIDER_CRYPTOPRO_CLOUD_CSP = 'CRYPTO_PRO_CLOUD_CSP';
+
+    /**
+     * ID сделки в CRM, в которую будет добавлена заявка
+     *
+     * @var int
+     */
+    public $opportunityId;
+
+    /**
+     * Массив площадок
+     *
+     * @var array
+     */
+    public $platforms;
+
+    /**
+     * Организационно-правовая форма
+     *
+     * @see self::CUSTOMER_LEGAL_FORM_LEGAL
+     * @var string
+     */
+    public $customerLegalForm;
+
+    /**
+     * Если передан, изменяется запись с указанным ID, иначе создается новая заявка
+     *
+     * @var int
+     */
+    public $id;
+
+    /**
+     * Криптопровайдер
+     *
+     * @see self::CRYPTOPROVIDER_CRYPTOPRO_2012
+     * @var string
+     */
+    public $cryptoProvider;
+
+    /**
+     * ID пользователя в CRM, который будет назначен владельцем. По умолчанию - владелец сделки
+     *
+     * @var int
+     */
+    public $ownerId;
+
+    /**
+     * Псевдоним заявки
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     * 1 - есть встроенная лицензия, 0 (по умолчанию) - нет
+     *
+     * @var bool
+     */
+    public $embededCP = false;
+}
