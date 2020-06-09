3d46479dc2ed2b317b376206bf8422784c0e84f8
n.zarubin <n.zarubin@itsirius.ru>
2020-06-09 13:24:19 +0300

    m

--- MockClient.php
+++ MockClient.php
@@ -5,13 +5,19 @@ namespace nikserg\CRMCertificateAPI;
 
 use nikserg\CRMCertificateAPI\models\data\Status;
 use nikserg\CRMCertificateAPI\models\request\ChangeStatus;
+use nikserg\CRMCertificateAPI\models\request\SendCheckRef;
 use nikserg\CRMCertificateAPI\models\request\SendCustomerForm as SendCustomerFormRequest;
 use nikserg\CRMCertificateAPI\models\request\SendCustomerFormData;
+use nikserg\CRMCertificateAPI\models\request\SendPrice;
 use nikserg\CRMCertificateAPI\models\response\BooleanResponse;
+use nikserg\CRMCertificateAPI\models\response\GetCheckRef;
 use nikserg\CRMCertificateAPI\models\response\GetCustomerForm;
 use nikserg\CRMCertificateAPI\models\response\GetOpportunity;
 use nikserg\CRMCertificateAPI\models\response\GetPassportCheck;
+use nikserg\CRMCertificateAPI\models\response\GetPrice;
 use nikserg\CRMCertificateAPI\models\response\GetSnilsCheck;
+use nikserg\CRMCertificateAPI\models\response\models\Platforms;
+use nikserg\CRMCertificateAPI\models\response\models\ProductTemplates;
 use nikserg\CRMCertificateAPI\models\response\SendCustomerForm as SendCustomerFormResponse;
 use nikserg\CRMCertificateAPI\models\request\SendCustomerForm;
 use nikserg\CRMCertificateAPI\models\response\Esia;
@@ -127,4 +133,102 @@ class MockClient extends Client
 
         return $response;
     }
+
+    public function getCheckRef(SendCheckRef $sendCheckRef)
+    {
+        $response = new GetCheckRef();
+        $response->id = 1;
+        $response->paymentMode = GetCheckRef::PAYMENT_UNLIMITED;
+        $response->userName = 'username';
+
+        return $response;
+    }
+
+    public function getPrice(SendPrice $sendPrice)
+    {
+        $result = json_decode('{
+    "productTemplates": [
+        {
+            "id": 981,
+            "price": "1500.00"
+        },
+        {
+            "id": 511,
+            "price": "1200.00"
+        },
+        {
+            "id": 127,
+            "price": "750"
+        },
+        {
+            "id": 1044,
+            "price": 1000
+        }
+    ],
+    "platforms": [
+        {
+            "price": 2500,
+            "name": "EPGU"
+        },
+        {
+            "price": 17600,
+            "name": "AETP_NEW_BASE,B2B,GPB,FABRIKANT"
+        },
+        {
+            "price": 3500,
+            "name": "AETP_NEW_BASE"
+        },
+        {
+            "price": 6900,
+            "name": "AETP_NEW_BASE,B2B"
+        },
+        {
+            "price": 8200,
+            "name": "AETP_NEW_BASE,FABRIKANT"
+        },
+        {
+            "price": 9500,
+            "name": "AETP_NEW_BASE,GPB"
+        },
+        {
+            "price": 8300,
+            "name": "AETP_NEW_BASE,RJD"
+        },
+        {
+            "price": 6000,
+            "name": "CDT_BUYER"
+        },
+        {
+            "price": 33900,
+            "name": "AETP_NEW_BASE,CENTREAL,CDT_BUYER,ATC,FREETRADE,URALBIDIN,ESP,TENDER_UG,ALFALOT"
+        },
+        {
+            "price": 5000,
+            "name": "EPGU,ROSREESTR_LEGAL"
+        }
+    ],
+    "notFoundPlatforms": []
+}');
+
+        $response = new GetPrice();
+        $response->productTemplates = [];
+        $response->platforms = [];
+
+
+        foreach ($result->productTemplates ?? [] as $productTemplateRequest) {
+            $productTemplate = new ProductTemplates();
+            $productTemplate->id = $productTemplateRequest->id ?? '';
+            $productTemplate->price = $productTemplateRequest->price ?? '';
+            $response->productTemplates[] = $productTemplate;
+        }
+        foreach ($result->platforms ?? [] as $platformRequest) {
+            $platform = new Platforms();
+            $platform->name = $platformRequest->name ?? '';
+            $platform->price = $platformRequest->price ?? '';
+            $response->platforms[] = $platform;
+        }
+        $response->notFoundPlatforms = $result->notFoundPlatforms ?? [];
+        return $response;
+    }
+
 }
