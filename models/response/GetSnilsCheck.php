44150c871168a6c0f826575db8d994e9d1b6bf31
Лев Тарасов <l.tarasov@itcomgk.ru>
2020-08-17 14:59:47 +0300

    Дополнение ошибок проверки снилс;

--- models/response/GetSnilsCheck.php
+++ models/response/GetSnilsCheck.php
@@ -10,48 +10,37 @@ namespace nikserg\CRMCertificateAPI\models\response;
  */
 class GetSnilsCheck
 {
-
     /**
-     * @var  string
      * Запись Esia не найдена
      */
     const STATUS_NOT_FOUND = 'not_found';
-
     /**
-     * @var  string
      *  Комбинация СНИЛС-ФИО-пол-дата рождения -  верные
      */
     const STATUS_SUCCESS = 'success';
     /**
-     * @var  string
      * Комбинация СНИЛС-ФИО-пол-дата рождения - неверная
      */
     const STATUS_WRONG = 'wrong';
-
     /**
-     * @var  string
      * Запрос отправлен, но ответ не получен
      */
     const STATUS_WAIT = 'wait';
-
     /**
-     * @var  string
      * Ошибка разбора ответа
      */
     const STATUS_PARSE_ERROR = 'parse_error';
-
     /**
-     * @var  string
      * Запрос в ESIA выдал ошибку
      */
     const STATUS_INTERNAL_ERROR = 'internal_error';
 
-
     const ERROR_MESSAGE_USER = [
-        self::STATUS_NOT_FOUND => 'Не найдено',
-        self::STATUS_WRONG => 'Комбинация СНИЛС-ФИО-пол-дата рождения - неверная',
-        self::STATUS_WAIT  => 'Запрос отправлен, но ответ не получен',
-        self::STATUS_PARSE_ERROR => 'Ошибка разбора ответа',
+        self::STATUS_INTERNAL_ERROR => 'Внутренняя ошибка сервиса проверки СНИЛС',
+        self::STATUS_NOT_FOUND      => 'Не найдено',
+        self::STATUS_WRONG          => 'Комбинация данных неверная',
+        self::STATUS_WAIT           => 'Запрос отправлен, но ответ не получен',
+        self::STATUS_PARSE_ERROR    => 'Ошибка разбора ответа',
     ];
 
     /**
@@ -60,14 +49,12 @@ class GetSnilsCheck
      * @var string
      */
     public $createRequestDate;
-
     public $status;
-
     public $message;
-
     public $id;
 
-    public function getErrorMessage() {
-        return self::ERROR_MESSAGE_USER[$this->status] ?? 'Тип ошибки для статуса не надена';
+    public function getErrorMessage()
+    {
+        return self::ERROR_MESSAGE_USER[$this->status] ?? "Неожиданный статус проверки: $this->status";
     }
 }
